04f78a422802aa08a8fd15c7a459226899972862
Christian Demsar <cdemsar3@gatech.edu>
2016-11-20 20:53:56 -0500

    Add some models to describe reports, events, odors

---
+++ android/app/src/main/java/models/Odor.java
@@ -0,0 +1,37 @@
+package models;
+
+/**
+ * Odor describes an environmental odor as experienced by an individual.
+ */
+public class Odor {
+    private final Strength strength;
+    private final Type type;
+    private final String description;
+
+    public Odor(Strength strength, Type type, String description) {
+        this.strength = strength;
+        this.type = type;
+        this.description = description;
+    }
+
+    public enum Strength {
+        LIGHT,          // "Barely noticeable."
+        MODERATE,       // "You can smell it, but it doesn't affect normal life."
+        STRONG,         // "Can't go outside."
+        VERY_STRONG     // "Makes  you feel sick."
+    }
+
+    public enum Type {
+        FRAGRANT,
+        PUNGENT,
+        PUTRID,
+        FECAL,
+        FISHY,
+        EARTHY,
+        PINE,
+        CHEMICAL,
+        MEDICINAL,
+        SULFUR,
+        OTHER
+    }
+}
---
+++ android/app/src/main/java/models/User.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.Date;
+
+public class User {
+    /* // Things that may or may not be useful to us.
+    public enum Sex {MALE, FEMALE, NOT_AVAILABLE}
+
+    private Date birthday; // TODO: age range instead of actual DOB?
+    private Sex sex;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private String username;
+    */
+}
