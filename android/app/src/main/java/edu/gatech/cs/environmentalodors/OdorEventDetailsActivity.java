643a138ed7a51604c345eccaf12b63b9eda6e4aa
Christian Demsar <cdemsar3@gatech.edu>
2016-11-28 21:22:01 -0500

    Remove unused imports

--- android/app/src/main/java/edu/gatech/cs/environmentalodors/OdorEventDetailsActivity.java
+++ android/app/src/main/java/edu/gatech/cs/environmentalodors/OdorEventDetailsActivity.java
@@ -2,15 +2,11 @@ package edu.gatech.cs.environmentalodors;
 
 import android.os.Bundle;
 import android.os.ParcelUuid;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.UUID;
 
