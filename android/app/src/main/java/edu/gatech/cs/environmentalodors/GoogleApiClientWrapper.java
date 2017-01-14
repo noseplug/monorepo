6d26d5855405f9430014a5d863431b652acbf73c
crasm <crasm@git.1.email.vczf.io>
2017-01-13 23:14:39 -0500

    Add permission check so lack of location access doesn't crash the app

--- android/app/src/main/java/edu/gatech/cs/environmentalodors/GoogleApiClientWrapper.java
+++ android/app/src/main/java/edu/gatech/cs/environmentalodors/GoogleApiClientWrapper.java
@@ -1,10 +1,13 @@
 package edu.gatech.cs.environmentalodors;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -24,9 +27,11 @@ class GoogleApiClientWrapper implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = GoogleApiClientWrapper.class.getSimpleName();
 
-    private GoogleApiClient googleApiClient;
+    private final GoogleApiClient googleApiClient;
+    private final Context ctx;
 
     public GoogleApiClientWrapper(Context ctx) {
+        this.ctx = ctx;
         googleApiClient = new GoogleApiClient.Builder(ctx)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -39,6 +44,7 @@ class GoogleApiClientWrapper implements GoogleApiClient.ConnectionCallbacks,
     public void onStart() {
         googleApiClient.connect();
     }
+
     public void onStop() {
         googleApiClient.disconnect();
     }
@@ -47,11 +53,22 @@ class GoogleApiClientWrapper implements GoogleApiClient.ConnectionCallbacks,
     public void onConnected(@Nullable Bundle bundle) {
         Log.v(TAG, "Connected");
 
-        // TODO: Make sure this doesn't crap out if location (wifi-based) is not turned on.
-        // TODO: Only navigate to the current location on first startup instead of every
-        // time we connect to Google.
+        if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.e(TAG, "No location permissions are available");
+            return;
+        }
         Location temp = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
 
+        // TODO: Only navigate to the current location on first startup instead of every
+        // time we connect to Google.
         if (temp == null) {
             Log.w(TAG, "No location returned from Google");
         } else {
@@ -70,15 +87,4 @@ class GoogleApiClientWrapper implements GoogleApiClient.ConnectionCallbacks,
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.e(TAG, "Connection failed: " + connectionResult);
     }
-/*
-    public static com.google.android.gms.maps.model.LatLng latLngFromLocation(android.location.Location l) {
-        com.google.android.gms.maps.model.LatLng ret
-            = new com.google.android.gms.maps.model.LatLng(
-                l.getLatitude(),
-                l.getLongitude()
-            );
-
-        return ret;
-    }
-    */
 }
