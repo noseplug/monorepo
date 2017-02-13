fd7b87b7ae9c652d9b26bbba1874159b2e690579
crasm <crasm@git.1.email.vczf.io>
2017-02-12 20:11:08 -0500

    Revert "Revert "Merge pull request #31 from noseplug/database""
    This reverts commit 104888164eb0ead71141e13f167306cd659d3e6f.

--- android/app/src/main/java/edu/gatech/cs/environmentalodors/OdorEventDetailsActivity.java
+++ android/app/src/main/java/edu/gatech/cs/environmentalodors/OdorEventDetailsActivity.java
@@ -13,6 +13,7 @@ import android.widget.ListView;
 
 import java.util.UUID;
 
+import edu.gatech.cs.environmentalodors.database.OfflineApi;
 import edu.gatech.cs.environmentalodors.models.OdorEvent;
 import edu.gatech.cs.environmentalodors.models.OdorReport;
 
@@ -33,7 +34,7 @@ public class OdorEventDetailsActivity extends AppCompatActivity
         setSupportActionBar((Toolbar) this.findViewById(R.id.toolbar));
 
         UUID odorEventId = ((ParcelUuid) getIntent().getParcelableExtra(ODOR_EVENT_ID)).getUuid();
-        odorEvent = ApplicationState.getInstance().getOdorEvent(odorEventId);
+        odorEvent = OfflineApi.noseplug.getOdorEvent(odorEventId);
         String description = odorEvent.getOdorReports().get(0).odor.description;
 
         Log.v(TAG, String.format("Starting activity with odor event %s (%s)",
--- android/app/src/main/java/edu/gatech/cs/environmentalodors/OdorReportDetailsActivity.java
+++ android/app/src/main/java/edu/gatech/cs/environmentalodors/OdorReportDetailsActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import java.util.UUID;
 
+import edu.gatech.cs.environmentalodors.database.OfflineApi;
 import edu.gatech.cs.environmentalodors.models.OdorReport;
 
 import static edu.gatech.cs.environmentalodors.IntentExtraNames.ODOR_REPORT_ID;
@@ -22,7 +23,7 @@ public class OdorReportDetailsActivity extends AppCompatActivity {
 
         UUID odorReportId = ((ParcelUuid) getIntent().getParcelableExtra(ODOR_REPORT_ID)).getUuid();
         Log.d("asd", odorReportId.toString());
-        OdorReport odorReport = ApplicationState.getInstance().getOdorReport(odorReportId);
+        OdorReport odorReport = OfflineApi.noseplug.getOdorReport(odorReportId);
 
         ((TextView) findViewById(R.id.odor_report_details_textview))
                 .setText(odorReport.toString());
