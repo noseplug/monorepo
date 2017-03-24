2d025cfd21e244ce60a65e382e6a44ccc121ce39
crasm <crasm@git.email.vczf.io>
2017-03-23 22:22:05 -0400

    Add initial attempt to write the notify-on-new-odor-event function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+admin.initializeApp(functions.config().firebase)
+
+exports.notifyGlobalNewOdorEvent = functions.database.ref('/events/{id}').onWrite(event => {
+  const ref = event.data.ref
+
+  const odorEventName = ref.name.val()
+  console.log('Odor Event Name: ' + odorEventName)
+  const odorEventOwner = ref['owner-id'].val()
+
+  const payload = {
+    notification: {
+      title: 'New Odor Event: ' + odorEventName,
+      body: odorEventOwner + ' reported ' + odorEventName
+    }
+  }
+
+  admin.messaging().sendToTopic('newOdorEvents', payload)
+
+  return null
+})
