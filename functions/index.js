a21862465b82fcdf6e739a734a19dbb96eef7365
crasm <crasm@git.email.vczf.io>
2017-03-24 11:49:33 -0400

    Notifications only appear for new odor events

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,13 @@ const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 
 exports.notifyGlobalNewOdorEvent = functions.database.ref('/events/{id}').onWrite(event => {
+  if (!event.data.exists() || event.data.previous.exists()) {
+    // TODO: Check if this is inefficient because it's triggered on
+    // every write to every event. There might be a way to listen only
+    // to new events by changing the path of the database ref above.
+    return null
+  }
+
   var odorEventName = event.data.child('name').val()
   var odorEventOwner = event.data.child('owner-id').val()
 
