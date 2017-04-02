5d10eb7a31ccf86037d001d5f1129452b8e08366
crasm <crasm@git.email.vczf.io>
2017-04-02 03:03:22 -0400

    Notify about odor events by a radius

--- android/app/src/main/java/com/noseplugapp/android/database/NoseplugFirebaseMessagingService.java
+++ android/app/src/main/java/com/noseplugapp/android/database/NoseplugFirebaseMessagingService.java
@@ -9,76 +9,82 @@ import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.gson.Gson;
+import com.noseplugapp.android.App;
 import com.noseplugapp.android.MapsActivity;
 import com.noseplugapp.android.R;
+import com.noseplugapp.android.utils.NoseplugLatLng;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Map;
-
-import static android.R.attr.data;
+import static java.lang.Math.*;
 
 public class NoseplugFirebaseMessagingService extends FirebaseMessagingService{
     private static final String TAG = NoseplugFirebaseMessagingService.class.getSimpleName();
+    private final App app = App.getInstance();
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, "Message received from: " + remoteMessage.getFrom());
-        if (remoteMessage.getData().size() <= 0) {
-            Log.e(TAG, "Missing data payload");
-            return;
-        }
-
-        JSONObject event;
         try {
-            event = new JSONObject(remoteMessage.getData().get("event"));
+            Log.d(TAG, "Message received from: " + remoteMessage.getFrom());
+            if (remoteMessage.getData().size() <= 0) {
+                Log.e(TAG, "Missing data payload");
+                return;
+            }
+
+            JSONObject event = new JSONObject(remoteMessage.getData().get("event"));
+            JSONObject report = new JSONObject(remoteMessage.getData().get("report"));
+            Log.d(TAG, "Message data.event: " + event);
+            Log.d(TAG, "Message data.report: " + report);
+
+            String content = report.getJSONObject("odor").toString();
+            String location = report.getString("location");
+
+            Gson gson = new Gson();
+            LatLng eventLocation = gson.fromJson(location, NoseplugLatLng.class).toGoogleLatLng();
+            Log.i(TAG, "New event occurred at " + eventLocation);
+
+            double distance = greatCircleDistanceKm(app.getUserLastKnownLocation(), eventLocation);
+            if (distance > 1000.0) {
+                Log.i(TAG, String.format("Event too far away (%f km), not notifying user", distance));
+                return;
+            }
+
+            Intent intent = new Intent(this, MapsActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                    PendingIntent.FLAG_ONE_SHOT);
+
+            Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.ic_add_white_24dp)
+                    .setContentTitle("New Odor Event")
+                    .setContentText(content)
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent);
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
         } catch (JSONException e) {
             e.printStackTrace();
-            Log.e(TAG, "Unable to get parse event payload");
-            return;
         }
+    }
 
-        JSONObject report;
-        try {
-            report = new JSONObject(remoteMessage.getData().get("report"));
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Log.e(TAG, "Unable to get parse report payload");
-            return;
-        }
-
-        Log.d(TAG, "Message data.event: " + event);
-        Log.d(TAG, "Message data.report: " + report);
-
-        String content;
-        try {
-            content = report.getJSONObject("odor").toString();
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Log.e(TAG, "Couldn't get new event report data");
-            return;
-        }
-
-        Intent intent = new Intent(this, MapsActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_add_white_24dp)
-                .setContentTitle("New Odor Event")
-                .setContentText(content)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    // Calculate the great-circle distance between two coordinates in kilometers.
+    // Approx radius of the earth => 6371 km
+    // https://en.wikipedia.org/wiki/Great-circle_distance
+    private static double greatCircleDistanceKm(LatLng a, LatLng b) {
+        return 6371.0 * acos(
+                sin(a.latitude) * sin(b.latitude) +
+                cos(a.latitude) * cos(b.latitude) * cos(abs(a.longitude - b.longitude))
+        );
     }
 }
